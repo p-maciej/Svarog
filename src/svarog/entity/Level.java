f13052103cbca113a47819f3b5f7018fc2d7e45a
JakubGolebiowski <jakubgolebiowskiofficial@gmail.com>
2020-03-24 10:14:00 +0100

    Adding Level and updating XP classes

---
+++ src/svarog/entity/Level.java
@@ -0,0 +1,17 @@
+package svarog.entity;
+
+public class Level {
+	private int level = 0;
+	
+	public Level(int level) {
+		this.level = level;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+}
--- src/svarog/entity/XP.java
+++ src/svarog/entity/XP.java
@@ -2,9 +2,18 @@ package svarog.entity;
 
 public class XP {
 	private int xp = 0;
+	private Level level= new Level(0);
+	
+	private int[] level_barriers = {0,20,40,80,160,320};
 	
 	public XP(int xp) {
 		this.xp = xp;
+		CheckLvlStatus();
+	}
+	
+	public void setXP(int xp) {
+		this.xp = xp;
+		CheckLvlStatus();
 	}
 	
 	public int GetXP() {
@@ -13,5 +22,18 @@ public class XP {
 	
 	public void AddXP(int points) {
 		xp = xp + points;
+		CheckLvlStatus();
+	}
+	
+	private void CheckLvlStatus() {
+		int iter = 1;
+		for(int i: level_barriers) {
+			if(this.xp > i) {
+				this.level.setLevel(iter);
+			}else {
+				break;
+			}
+			iter++;
+		}
 	}
 }
