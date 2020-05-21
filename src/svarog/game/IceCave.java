05c8523a9cea19f4963c0799421a9b1a850a04b3
Patryk Gora <patryk111g@wp.pl>
2020-05-21 10:45:38 +0200

    Adding textures, enemies and NPCs on map.

--- src/svarog/game/IceCave.java
+++ src/svarog/game/IceCave.java
@@ -1,9 +1,14 @@
 package svarog.game;
 
+import svarog.entity.Enemy;
+import svarog.entity.EntityItem;
+import svarog.entity.NPC;
 import svarog.entity.Player;
 import svarog.io.Window;
 import svarog.render.Camera;
 import svarog.render.Texture;
+import svarog.save.EntityHolder;
+import svarog.save.Save;
 import svarog.world.Door;
 import svarog.world.Tile;
 import svarog.world.World;
@@ -14,7 +19,18 @@ abstract class IceCave {
 		world.loadMap("iceCave.png", 32);
 		world.setSolidTilesFromMap("iceCave_mask.png");
 		
-
+		for(EntityHolder i: Save.getEntityHolder04()) {
+			if(i.getType().equals("npc")) {
+				world.addEntity(new NPC(i));
+			}else if(i.getType().equals("enemy")) {
+				world.addEntity(new Enemy(i));
+			}else if(i.getType().equals("entityItem")){
+				world.addEntity(new EntityItem(i));
+			}else {
+				System.out.println("WTF???");
+			}
+		}
+		
 		world.addEntity(player);
 		//player.setPosition(1, 53);
 		
