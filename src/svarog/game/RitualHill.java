95a7aa8603cc0bbecb9377f034a408f283b5d6dc
Patryk Gora <patryk111g@wp.pl>
2020-05-21 16:01:16 +0200

    Adding enemies and npc to location RitualHill,village, fix bugs in .lang

--- src/svarog/game/RitualHill.java
+++ src/svarog/game/RitualHill.java
@@ -1,9 +1,16 @@
 package svarog.game;
 
+import java.util.List;
+
+import svarog.entity.Enemy;
+import svarog.entity.EntityItem;
+import svarog.entity.NPC;
 import svarog.entity.Player;
 import svarog.io.Window;
 import svarog.render.Camera;
 import svarog.render.Texture;
+import svarog.save.EntityHolder;
+import svarog.save.Save;
 import svarog.world.Door;
 import svarog.world.Tile;
 import svarog.world.World;
@@ -14,6 +21,19 @@ public abstract class RitualHill {
 		world.loadMap("ritualHill_night.png", 32);
 		world.setSolidTilesFromMap("ritualHill_mask.png");
 		
+		List<EntityHolder> temp = Save.ReadWorldEntities("world06");
+		
+		for(EntityHolder i: temp) {
+			if(i.getType().equals("npc")) {
+				world.addEntity(new NPC(i));
+			}else if(i.getType().equals("enemy")) {
+				world.addEntity(new Enemy(i));
+			}else if(i.getType().equals("entityItem")){
+				world.addEntity(new EntityItem(i));
+			}else {
+				System.out.println("WTF???");
+			}
+		}
 
 		world.addEntity(player);
 		
