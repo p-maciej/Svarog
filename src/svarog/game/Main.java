868c968d7183dbd916f71429cce55a90d3f18f44
Maciej Pawlak <maciejpawlak.pl@gmail.com>
2020-06-08 18:13:32 +0200

    Updated version.

--- src/svarog/game/Main.java
+++ src/svarog/game/Main.java
@@ -113,7 +113,7 @@ public class Main {
 	//Path Finder
 	private static PathFinder pathFinder;
 	
-	public static final String VERSION = "0.1"; 
+	public static final String VERSION = "1.0 BETA"; 
 	
 	private static void windowInit() {
 		window = new Window();
